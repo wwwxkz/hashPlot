9f0090c067ebfee62de34bf8b806cd915ff30a28
wwwxkz <wxkzwin@gmail.com>
2022-08-10 13:33:34 -0300

    Added interactive shell and refactor plotter into a class

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # Hash Plot
 # by wwwxkz
-# github.com/wwwxkz 
+# github.com/wwwxkz
 # gitlab.com/wwwxkz
 # linkedin.com/wwwxkz
 
 # Programming with no goal, thought about making an program to
 # state questions (situations) and options to adress those like
-# like an notepad, but "funnier" 
+# like an notepad, but "funnier"
 
 # It was a cool, but as I began I tought that making a geogebra
 # in the console would be even better
 
 # In python just because it is easy, and I was playing around with
-# the interactive shell 
+# the interactive shell
 
 # TODO
+# - add bar_h to verticalhorizontal_bar_graph
 # - improve function logic
 # - could _bar and _vertical_bar be the same func?
 # - improve bar graph legend logic
@@ -24,202 +25,215 @@
 # - multiple screens
 # - change func in the interactive shell
 
-def _display(table_w, frame):
-    print('-' * table_w)
-    for line in frame:
-        print('|' + line + '|')
-    print('-' * table_w)
+class Plotter:
+    def display(self, table_w, frame):
+        print('-' * table_w)
+        for line in frame:
+            print('|' + line + '|')
+        print('-' * table_w)
 
-def _bar_graph(table_w, bar_w, columns):
-    frame = []
-    for column in columns:
-        for line in range(column[0]):
+    def horizontal_bar_graph(self, bar_w, columns):
+        frame = []
+        for column in columns:
+            for line in range(column[0]):
+                try:
+                    frame[line] += ' ' + '#' * bar_w
+                except:
+                    frame.insert(line, ' ' + '#' * bar_w)
+        for c, column in enumerate(columns):
             try:
-                frame[line] += ' ' + '#' * bar_w
-            except:
-                frame.insert(line, ' ' + '#' * bar_w)
-    for c, column in enumerate(columns):
-        try:
-            if c == 0:
-                frame[:0] = ' ' 
-                frame[0] = ' ' + column[1]
-            else:
-                frame[0] += ' ' + column[1]
-        except:
-            frame.insert(0, ' ' + column[1])
-    # Reverse frame as array starts from top and frames and graphs from bottom
-    return frame[::-1]
-
-def _vertical_bar_graph(table_w, bar_h, rows):
-    frame = []
-    # Find the largest label
-    l_label = 0
-    for row in rows:
-        if len(row[1]) > l_label:
-            l_label = len(row[1])
-    for i, row in enumerate(rows):
-        try:
-            frame[i] += '#' * row[0]
-        except:
-            label = row[1] + ' ' * (int(l_label) - int(len(row[1])))
-            frame.insert(i, label + ': ' + '#' * row[0])
-    return frame[::-1]
-
-def _pizza_graph(table_w, table_h, slices):
-    frame = []
-    reverse = False
-    for slice in slices:
-        # Create circle
-        for c, line in enumerate(range(table_h)):
-            if c == table_h/2:
-                reverse = True
-            if reverse == True:
-                c = table_h - c
-            try:
-                frame[line] += '@'
-            except:
-                frame.insert(line, '@' * (2 * c))
-    return frame
-
-def _function_graph(table_w, scale, function):
-    frame = []
-    # (0,0) in cartesian plane
-    zero = round((scale * 2) / 2)
-    # Get function positions (x,y) based on scale
-    res_y = []
-    # Positive and negative (exmp -> -5 left +5 right)
-    # +1 to add 0 position
-    for p, _ in enumerate(range((scale * 2) + 1)):
-        # Replacing x by scale of the cartesian plane 
-        # Using y for the sake of practicality
-        # as both y and x are the same there is no problem
-        replace_x = str(function.replace('x', str(p)))
-        res_y.append(eval(replace_x))
-    print('Points: ', res_y)
-    # Create y axis
-    for y, point in enumerate(range((scale * 2) + 1)):
-        frame.append('')      
-        # Creates x axis
-        for x, pointer in enumerate(range((scale * 2) + 1)):
-            if x == zero:
-                frame[y] += '|'
-            else:
-                if y == zero:
-                    frame[y] += '-'
+                if c == 0:
+                    frame[:0] = ' '
+                    frame[0] = ' ' + column[1]
                 else:
-                    frame[y] += ' '
-        # Does the function pass thought this point?
-        # Else, default values |, -, ' '
-        for i in res_y:
-            if y == i:
-                frame[y] = frame[y][:i] + '@' + frame[y][i:]
-                frame[y] = frame[y].replace('@ ', '@')
-                # Are we at the center?
-                if y == zero:
-                    frame[y] = frame[y].replace('@|', '@')
-    return frame
+                    frame[0] += ' ' + column[1]
+            except:
+                frame.insert(0, ' ' + column[1])
+        # Reverse frame as array starts from top and frames and graphs from bottom
+        return frame[::-1]
 
-def _align(table_w, frame, direction):
-    for i, line in enumerate(frame):
-        spaces = int(table_w - len(line)) - 2
-        if direction == 'r':
-            frame[i] = ' ' * spaces + line
-        elif direction == 'l':
-            frame[i] = line + ' ' * spaces
-        elif direction ==  'c':
-            frame[i] = ' ' * int(spaces/2) + line + ' ' * int(spaces/2)
-        else:
-            print('Invalid direction')
-    return frame
+    def vertical_bar_graph(self, rows):
+        frame = []
+        # Find the largest label
+        l_label = 0
+        for row in rows:
+            if len(row[1]) > l_label:
+                l_label = len(row[1])
+        for i, row in enumerate(rows):
+            try:
+                frame[i] += '#' * row[0]
+            except:
+                label = row[1] + ' ' * (int(l_label) - int(len(row[1])))
+                frame.insert(i, label + ': ' + '#' * row[0])
+        return frame[::-1]
 
-# EXAMPLE DISPLAY TEXT
-def _exmp_text():
-    # Set table (display) size as a var for convenience
-    table_w = 40
-    # Display anything you want
-    frame = [
-        'True?',
-        'Small question',
-        'Trying bigger question',
-        'Is that even a question?'
-    ]
-    # Align text to R, L, C
-    frame = _align(table_w, frame, 'r')
-    # Pass desired frame and table (display) size
-    _display(table_w, frame)
+    def pizza_graph(self, table_h, slices):
+        frame = []
+        reverse = False
+        for slice in slices:
+            # Create circle
+            for c, line in enumerate(range(table_h)):
+                if c == table_h/2:
+                    reverse = True
+                if reverse == True:
+                    c = table_h - c
+                try:
+                    frame[line] += '@'
+                except:
+                    frame.insert(line, '@' * (2 * c))
+        return frame
 
-# EXAMPLE BAR GRAPH
-def _exmp_bar():
-    # Set bar graph columns as tuple matrixes 
-    table_w = 40
-    columns = [
-        (4, 'Russia'),
-        (7, 'Ukraine'),
-        (4, 'Argentina'),
-        (2, 'Japan'),
-        (9, 'Canada')
-    ]
-    frame = _bar_graph(table_w, 4, columns)
-    frame = _align(table_w, frame, 'l')
-    _display(table_w, frame)
+    def function_graph(self, scale, function):
+        frame = []
+        # (0,0) in cartesian plane
+        zero = round((scale * 2) / 2)
+        # Get function positions (x,y) based on scale
+        res_y = []
+        # Positive and negative (exmp -> -5 left +5 right)
+        # +1 to add 0 position
+        for p, _ in enumerate(range((scale * 2) + 1)):
+            # Replacing x by scale of the cartesian plane
+            # Using y for the sake of practicality
+            # as both y and x are the same there is no problem
+            replace_x = str(function.replace('x', str(p)))
+            res_y.append(eval(replace_x))
+        print('Points: ', res_y)
+        # Create y axis
+        for y, point in enumerate(range((scale * 2) + 1)):
+            frame.append('')
+            # Creates x axis
+            for x, pointer in enumerate(range((scale * 2) + 1)):
+                if x == zero:
+                    frame[y] += '|'
+                else:
+                    if y == zero:
+                        frame[y] += '-'
+                    else:
+                        frame[y] += ' '
+            # Does the function pass thought this point?
+            # Else, default values |, -, ' '
+            for i in res_y:
+                if y == i:
+                    frame[y] = frame[y][:i] + '@' + frame[y][i:]
+                    frame[y] = frame[y].replace('@ ', '@')
+                    # Are we at the center?
+                    if y == zero:
+                        frame[y] = frame[y].replace('@|', '@')
+        return frame
 
-  
-# EXAMPLE PIZZA GRAPH
-def _exmp_pizza():
-    table_w = 40
-    table_h = 10
-    slices = [
-        (20, 'New Zeland'),
-        (50, 'Australia'),
-        (10, 'China'),
-        (80, 'Mongolia'),
-        (60, 'Germany'),
-        (50, 'Poland')
-    ]
-    frame = _pizza_graph(table_w, table_h, slices)
-    frame = _align(table_w, frame, 'c')
-    _display(table_w, frame)
+    def align(self, table_w, frame, direction):
+        for i, line in enumerate(frame):
+            spaces = int(table_w - len(line)) - 2
+            if direction == 'r':
+                frame[i] = ' ' * spaces + line
+            elif direction == 'l':
+                frame[i] = line + ' ' * spaces
+            elif direction == 'c':
+                frame[i] = ' ' * int(spaces/2) + line + ' ' * int(spaces/2)
+            else:
+                print('Invalid direction')
+        return frame
 
-# EXAMPLE VERTICAL BAR GRAPH
-def _exmp_vertical_bar():
-    table_w = 40
-    rows = [
-        (4, 'Russia'),
-        (7, 'Ukraine'),
-        (4, 'Argentina'),
-        (2, 'Japan'),
-        (9, 'Canada')
-    ]
-    frame = _vertical_bar_graph(table_w, 2, rows)
-    frame = _align(table_w, frame, 'l')
-    _display(table_w, frame)
+    # EXAMPLE DISPLAY TEXT
+    def example_text(self):
+        # Set table (display) size as a var for convenience
+        table_w = 40
+        # Display anything you want
+        frame = [
+            'True?',
+            'Small question',
+            'Trying bigger question',
+            'Is that even a question?'
+        ]
+        # Align text to R, L, C
+        frame = self.align(table_w, frame, 'r')
+        # Pass desired frame and table (display) size
+        self.display(table_w, frame)
 
-def _exmp_function():
-    table_w = 40
-    function = 'x+x'
-    # 5 to left, right, top, bottom from 0
-    cartesian_plane = 5
-    frame = _function_graph(table_w, cartesian_plane, function)
-    frame = _align(table_w, frame, 'c')
-    _display(table_w, frame)
-    # Another function
-    function = 'x+1'
-    frame = _function_graph(table_w, cartesian_plane, function)
-    frame = _align(table_w, frame, 'c')
-    _display(table_w, frame)
-    # One more
-    function = '2'
-    frame = _function_graph(table_w, cartesian_plane, function)
-    frame = _align(table_w, frame, 'c')
-    _display(table_w, frame)
-    # Last one I promise
-    function = 'x - x * 2'
-    frame = _function_graph(table_w, cartesian_plane, function)
-    frame = _align(table_w, frame, 'c')
-    _display(table_w, frame)
+    def example_horizontal_bar(self):
+        # Set bar graph columns as tuple matrixes
+        table_w = 40
+        columns = [
+            (4, 'Russia'),
+            (7, 'Ukraine'),
+            (4, 'Argentina'),
+            (2, 'Japan'),
+            (9, 'Canada')
+        ]
+        frame = self.horizontal_bar_graph(4, columns)
+        frame = self.align(table_w, frame, 'l')
+        self.display(table_w, frame)
 
-_exmp_text()
-_exmp_bar()
-_exmp_pizza()
-_exmp_vertical_bar()
-_exmp_function()
+    def example_pizza(self):
+        table_w = 40
+        table_h = 10
+        slices = [
+            (20, 'New Zeland'),
+            (50, 'Australia'),
+            (10, 'China'),
+            (80, 'Mongolia'),
+            (60, 'Germany'),
+            (50, 'Poland')
+        ]
+        frame = self.pizza_graph(table_h, slices)
+        frame = self.align(table_w, frame, 'c')
+        self.display(table_w, frame)
+
+    def example_vertical_bar(self):
+        table_w = 40
+        rows = [
+            (4, 'Russia'),
+            (7, 'Ukraine'),
+            (4, 'Argentina'),
+            (2, 'Japan'),
+            (9, 'Canada')
+        ]
+        frame = self.vertical_bar_graph(rows)
+        frame = self.align(table_w, frame, 'l')
+        self.display(table_w, frame)
+
+    def example_function(self):
+        table_w = 40
+        function = 'x+x'
+        # 5 to left, right, top, bottom from 0
+        cartesian_plane = 5
+        frame = self.function_graph(cartesian_plane, function)
+        frame = self.align(table_w, frame, 'c')
+        self.display(table_w, frame)
+        # Another function
+        function = 'x+1'
+        frame = self.function_graph(cartesian_plane, function)
+        frame = self.align(table_w, frame, 'c')
+        self.display(table_w, frame)
+        # One more
+        function = '2'
+        frame = self.function_graph(cartesian_plane, function)
+        frame = self.align(table_w, frame, 'c')
+        self.display(table_w, frame)
+        # Last one I promise
+        function = 'x - x * 2'
+        frame = self.function_graph(cartesian_plane, function)
+        frame = self.align(table_w, frame, 'c')
+        self.display(table_w, frame)
+
+    def examples(self):
+        self.example_text()
+        self.example_horizontal_bar()
+        self.example_pizza()
+        self.example_vertical_bar()
+        self.example_function()
+
+
+def menu():
+    p = Plotter()
+    print('Examples : e')
+    print('Clear    : c')
+    while 1:
+        option = input('@-->> ')
+        if (option == 'e'):
+            p.examples()
+        if (option == 'c'):
+            print("\033[H\033[J", end="")
+
+
+menu()
